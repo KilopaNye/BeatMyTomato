5463a3195275dfa4517efbd0df9d2b43de099e2e
Nye <steve69988@gmail.com>
2026-09-07 19:52:52 +0800

    feat: add console test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("BeatMyTomato skeleton ready.");
+using BeatMyTomato.Enums;
+using BeatMyTomato.Services;
+
+var sm = new TimerStateMachine(SessionState.Idle);
+Console.WriteLine(sm.CurrentState);       // Idle
+sm.Fire(SessionTrigger.Start);
+Console.WriteLine(sm.CurrentState);       // Running
+sm.Fire(SessionTrigger.Complete);
+Console.WriteLine(sm.CurrentState);       // Completed
+sm.Fire(SessionTrigger.Pause);            // 這裡應該會丟出例外，程式會中斷並印出 stack trace
