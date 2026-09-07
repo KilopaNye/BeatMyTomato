ffdb7fd9aacc6c28293d346f3b6c49a4e8b01b7f
Nye <steve69988@gmail.com>
2026-09-07 19:52:52 +0800

    feat: add console test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("BeatMyTomato skeleton ready.");
+using BeatMyTomato.Enums;
+using BeatMyTomato.Services;
+
+var sm = new TimerStateMachine(SessionState.Idle);
+Console.WriteLine(sm.CurrentState);       // Idle
+sm.Fire(SessionTrigger.Start);
+Console.WriteLine(sm.CurrentState);       // Running
+sm.Fire(SessionTrigger.Complete);
+Console.WriteLine(sm.CurrentState);       // Completed
+sm.Fire(SessionTrigger.Pause);            // Exception
